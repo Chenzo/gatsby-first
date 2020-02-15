59bef3256544f0dbf78106b4a3e445c9cdd53bdf
Vince Suriani <chenzorama@hotmail.com>
2020-02-15 09:04:59 -0500

    Blog Works

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+//import Image from "../components/image"
+//import SEO from "../components/seo"
 export default ({ data }) => {
   console.log(data)
   return (
@@ -13,13 +12,15 @@ export default ({ data }) => {
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <h3>
-              {node.frontmatter.title}{" "}
-              <span>
-                — {node.frontmatter.date}
-              </span>
-            </h3>
-            <p>{node.excerpt}</p>
+            <Link to={node.fields.slug}>
+              <h3>
+                {node.frontmatter.title}{" "}
+                <span>
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+              </Link>
           </div>
         ))}
       </div>
@@ -36,6 +37,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
