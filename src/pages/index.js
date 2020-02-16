c285a66f2ea3401d0e5fa6f4dae9b14a0ab6a58e
Vince Suriani <chenzorama@hotmail.com>
2020-02-16 10:15:40 -0500

    Got image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
+//import Image from "../components/image"
 import SEO from "../components/seo"
+import Image from 'gatsby-image'
 export default ({ data }) => {
   return (
     <Layout>
       <SEO title="Home" />
       <div>
         <h1>Words</h1>
-        <Image />
+        <Image
+        className="avatar"
+        fixed={data.avatar.childImageSharp.fixed}
+        alt="Mr. Chenzo"
+        style={{
+          marginBottom: 0,
+          minWidth: 150,
+          borderRadius: '100%',
+          border: '8px solid lavender',
+        }}
+        imgStyle={{
+          borderRadius: '50%',
+        }}
+      />
+
+        {data.avatar.childImageSharp.fixed.src}
 
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
@@ -44,6 +60,17 @@ export const query = graphql`
             slug
           }
           excerpt
+        }
+      }
+    }
+    avatar: file(absolutePath: { regex: "/avatar.png/" }) {
+      childImageSharp {
+        fixed(width: 150, height: 150, quality: 90) {
+          base64
+          width
+          height
+          src
+          srcSet
         }
       }
     }
